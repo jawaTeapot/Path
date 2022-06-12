11121fcfcd4fca8944731b8d75e576fca2726b97
jawaTeapot <78386140+jawaTeapot@users.noreply.github.com>
2022-06-12 23:07:20 +0300

    end

--- script/main.js
+++ script/main.js
@@ -11,3 +11,45 @@ $(document).ready(function(){
         nextArrow: "#cl_right"
     });
 });
+$( "#menu_doctor" ).click(function () {
+    if($("#doctor").is(":visible")){
+        $("#doctor").hide();
+        $("#nav").removeClass("nav--open");
+    } else {
+        $("#doctor").show();
+        $("#nav").addClass("nav--open");
+    }
+    $("#patient").hide();
+    $("#drug").hide();
+})
+$( "#menu_patient" ).click(function () {
+    if($("#patient").is(":visible")){
+        $("#patient").hide();
+        $("#nav").removeClass("nav--open");
+    } else {
+        $("#patient").show();
+        $("#nav").addClass("nav--open");
+    }
+    $("#doctor").hide();
+    $("#drug").hide();
+})
+$( "#menu_drug" ).click(function () {
+    if($("#drug").is(":visible")){
+        $("#drug").hide();
+        $("#nav").removeClass("nav--open");
+    } else {
+        $("#drug").show();
+        $("#nav").addClass("nav--open");
+    }
+    $("#doctor").hide();
+    $("#patient").hide();
+})
+$( document ).click( function (e) {
+    const a = $(e.target).closest('.nav')
+    if (a.length === 0){
+        $("#doctor").hide();
+        $("#patient").hide();
+        $("#drug").hide();
+        $("#nav").removeClass("nav--open");
+    }
+})
